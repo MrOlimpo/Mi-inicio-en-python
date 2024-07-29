ac9eb15aded17b1283ffb68d69da0c46d9078d6a
MrOlimpo <alfredopea849@gmail.com>
2024-07-29 16:47:46 -0500

    00

---
+++ 00_hello.py
@@ -0,0 +1,12 @@
+# Ey this is a comment Hello Mundo
+# Our Hello Mundo 
+print("Hello mundo!")
+
+# Consulter the type date 
+print(type("Im a date str")) # Type "str"
+print(type(5)) # Type "int"
+print(type(1.5)) # Type "float"
+print(type(2 + 4j)) # Type "complex" 
+print(type(True)) # Type "bool" 
+
+  
